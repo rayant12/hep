b168f320e38744568f38d6398de4bf1527417a77
syuvivida <syu@phy.ncu.edu.tw>
2016-05-13 21:57:52 +0800

    add comments

--- physics/eta.C
+++ physics/eta.C
@@ -1,26 +1,34 @@
+// include all necessary header files
 #include <TH1.h>
 #include <TRandom3.h>
 #include <TMath.h>
 #include <TFile.h>
+
 void eta()
 {
+  // declare and define two histograms (their number of bins and boundaries)
   TH1F* h_eta = new TH1F("h_eta","",100,-10,10);
   TH1F* h_theta = new TH1F("h_theta","",100,0,TMath::Pi());
   
+  // start producing distributions
+  // set the number of events to be 10000
   const int neve=10000;
   TRandom3* myrndm = new TRandom3();
+
   for(unsigned int i=0; i<neve; i++){
+    
+    // produce a flat theta distributions
     float theta = myrndm->Rndm()*TMath::Pi();
     h_theta->Fill(theta);
 
     float eta = -TMath::Log(TMath::Tan(0.5*theta));
-
     h_eta->Fill(eta);
   }
-
+  
   h_theta->Draw();
   h_eta->Draw();
 
+  // save the histograms in a root file
   TFile* outFile = new TFile("eta_theta.root","recreate");
   
   h_theta->Write();
